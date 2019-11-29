35feaa7266efad69af6ee60be9612eb0faa2511b
hwCloudDDS <shenming4@huawei.com>
2019-11-29 09:57:05 +0800

    add for stream

--- src/main/java/com/huaweicloud/nosql/streamfetcher/StreamFetcher.java
+++ src/main/java/com/huaweicloud/nosql/streamfetcher/StreamFetcher.java
@@ -1,6 +1,5 @@
 package com.huaweicloud.nosql.streamfetcher;
 
-import com.datastax.driver.core.Cluster;
 import com.datastax.driver.core.ColumnDefinitions;
 import com.datastax.driver.core.ColumnMetadata;
 import com.datastax.driver.core.DataType;
@@ -11,129 +10,12 @@ import com.huaweicloud.nosql.streamfetcher.req.DataItem;
 import com.huaweicloud.nosql.streamfetcher.req.RowInfo;
 import com.huaweicloud.nosql.streamfetcher.req.StreamInfo;
 import com.huaweicloud.nosql.streamfetcher.req.TableEvent;
-import com.huaweicloud.nosql.streamfetcher.utils.WrapperCassandraSession;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class StreamFetcher
 {
-    public static List<String> GetShardIterator(Cluster cluster, String keySpace, String tableName)
-        throws Exception
-    {
-        if (cluster == null || keySpace == null || tableName == null) {
-            System.out.println("request args are illegal \n");
-            return null;
-        }
-
-        List<String> streamShardList = new ArrayList<String>();
-
-        WrapperCassandraSession session = WrapperCassandraSession.getRetrySession(cluster);
-
-        String querySql =
-            "select stream_enabled, stream_shards, keyspace_name, table_name from system_schema.tables where keyspace_name = '"
-                + keySpace + "' and table_name = '" + tableName + "'";
-        ResultSet rs = null;
-        try {
-            rs = session.execute(querySql);
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        if (rs == null) {
-            session.close();
-            return streamShardList;
-        }
-
-        while (rs.iterator().hasNext()) {
-            Row one = rs.one();
-            streamShardList = (List<String>)one.getObject(1);
-            break;
-        }
-
-        session.close();
-        return streamShardList;
-    }
-
-
-    public static StreamInfo GetRecords(Cluster cluster, String keySpace, TableEvent tableEvent)
-        throws Exception
-    {
-        if (cluster == null || keySpace == null || tableEvent == null) {
-            System.out.println("request args are illegal \n");
-            return null;
-        }
-
-        WrapperCassandraSession session = WrapperCassandraSession.getRetrySession(cluster);
-
-        String table = tableEvent.getTable();
-        String startEventID = tableEvent.getEventID();
-        String shardID = tableEvent.getShardID();
-
-        String key = "\"" + keySpace + "\"" + "." + "\"" + table + "$streaming\"";
-
-        String querySql;
-        if (startEventID != null) {
-            querySql = "SELECT * FROM  " + key + " where \"@eventID\" > " + startEventID + " and \"@shardID\" = '"
-                + shardID + "' limit "+ tableEvent.getLimitRow();
-        }
-        else {
-            querySql = "SELECT * FROM  " + key + " where \"@shardID\" = '" + shardID + "' limit " + tableEvent.getLimitRow();
-        }
-
-        ResultSet rs = null;
-        try {
-            rs = session.execute(querySql);
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
-        if (rs == null) {
-            session.close();
-            return null;
-        }
-
-        ColumnDefinitions columnDefinitions = rs.getColumnDefinitions();
-        StreamInfo streamInfo = new StreamInfo();
-        streamInfo.setTable(table);
-        streamInfo.setShardID(shardID);
-
-        List<ColumnMetadata> primaryKey = cluster.getMetadata().getKeyspace(keySpace).getTable(table).getPrimaryKey();
-        RowInfo tmpRow = null;
-        while (rs.iterator().hasNext()) {
-            Row one = rs.one();
-            RowInfo row = parseColumn(one, columnDefinitions, table, primaryKey);
-            if (tmpRow == null) {
-                tmpRow = row;
-                continue;
-            }
-            if (!row.compare(tmpRow)) {
-                streamInfo.addColumn(tmpRow);
-                tmpRow = row;
-            } else {
-                if (row.getOldImage().size() == 0 && tmpRow.getOldImage().size() > 0) {
-                    row.setOldImage(tmpRow.getOldImage());
-                    streamInfo.addColumn(row);
-                    tmpRow = null;
-                }
-                if (row.getNewImage().size() == 0 && tmpRow.getNewImage().size() > 0) {
-                    row.setNewImage(tmpRow.getNewImage());
-                    streamInfo.addColumn(row);
-                    tmpRow = null;
-                }
-            }
-
-        }
-
-        if (tmpRow != null && !tmpRow.getOperateType().equals("UPDATE")) {
-            streamInfo.addColumn(tmpRow);
-        }
-        session.close();
-        return streamInfo;
-
-    }
-
     public static List<String> GetShardIterator(Session session, String keySpace, String tableName) {
         if (session == null || keySpace == null || tableName == null) {
             System.out.println("request args are illegal \n");
@@ -168,22 +50,25 @@ public class StreamFetcher
         String table = tableEvent.getTable();
         String startEventID = tableEvent.getEventID();
         String shardID = tableEvent.getShardID();
+        int limitRow = tableEvent.getLimitRow();
         List<ColumnMetadata> primaryKey = tableEvent.getPrimaryKey();
 
-        if (table == null || startEventID == null || shardID == null || primaryKey == null) {
+        if (table == null || shardID == null || primaryKey == null) {
             System.out.println("request tableEvent args are illegal \n");
             return null;
         }
+
+        limitRow = limitRow > 1000 ? 1000: limitRow;
 
         String key = "\"" + keySpace + "\"" + "." + "\"" + table + "$streaming\"";
 
         String querySql;
         if (startEventID != null) {
             querySql = "SELECT * FROM  " + key + " where \"@eventID\" > " + startEventID + " and \"@shardID\" = '"
-                + shardID + "' limit "+ tableEvent.getLimitRow();
+                + shardID + "' limit "+ limitRow;
         }
         else {
-            querySql = "SELECT * FROM  " + key + " where \"@shardID\" = '" + shardID + "' limit "+ tableEvent.getLimitRow();
+            querySql = "SELECT * FROM  " + key + " where \"@shardID\" = '" + shardID + "' limit "+ limitRow;
         }
 
         ResultSet rs = session.execute(querySql);
@@ -239,8 +124,8 @@ public class StreamFetcher
             if (name.equalsIgnoreCase("@eventID")) {
                 DataItem eventID = getValue(null, i, one, columnDefinitions, i);
                 row.setEventID(String.valueOf(eventID.getValue()));
-                System.out.println(
-                    "Get the column from eventID : " + eventID.getValue() + ". The table name is : " + table + ".");
+//                System.out.println(
+//                    "Get the column from eventID : " + eventID.getValue() + ". The table name is : " + table + ".");
                 continue;
             }
             if (name.equalsIgnoreCase("@newOldImage")) {
